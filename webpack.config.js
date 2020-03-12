212783977652ad1c38d0be09470cefe19ac419f3
EclecticTaco <calvi5210@gmail.com>
2020-03-11 17:27:14 -0700

    link webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,21 @@ var SRC_DIR = path.join(__dirname, '/client');
 var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+      filename: 'bundle.js',
+      path: DIST_DIR
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          loader: 'babel-loader',
+          options: {
+            'presets': ['@babel/preset-env', '@babel/preset-react']
+          }
         }
-      }
-    ]
-  }
-};
+      ]
+    }
+  };
+  
