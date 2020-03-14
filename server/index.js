d5af7af26468d3062a51ac5efcc4e59cb4fe9739
EclecticTaco <calvi5210@gmail.com>
2020-03-14 11:56:18 -0700

    fixed style

--- server/index.js
+++ server/index.js
@@ -1,37 +1,30 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-var Db = require('../database/index.js')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const Db = require('../database/index.js');
+
+const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Pinged')
-})
+  console.log('Pinged');
+});
 
-app.use(express.static(__dirname + '/../public'))
+app.use(express.static(path.join(__dirname, '/../public')));
 
 
-app.get('/api/carousel/', (req, res) => {
-    console.log('GET REQUEST recieved')
-    var restaurant = 20;
-    Db.getPictures(20, (err, results) => {
-        if (err) {
-            console.log('err in server CB: ', err) 
-        } else {
-            console.log('results in server CB: ', results)
-        }
-    })
-})
+app.get('/api/carousel/:restaurantID', (req, res) => {
+  const restaurant = req.params.restaurantID;
+  console.log('GET REQUEST recieved for restaurant ID of: ', restaurant);
+  Db.getPictures(restaurant, (err, results) => {
+    if (err) {
+      console.log('err in server CB: ', err); 
+    } else {
+      console.log('results in server CB: ', results);
+    }
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000')
-})
+  console.log('Server listening on port 3000');
+});
 
-/* 
-data: 20
-
-'/api/carousel/:restaurantID'
-
-req.params.restaurantID
-
-
-*/
