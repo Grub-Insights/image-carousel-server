0d3a8bd204e4fe156a8038607daaffd538356350
EclecticTaco <calvi5210@gmail.com>
2020-03-22 21:31:05 -0700

    app GET request route for restuant name

--- server/index.js
+++ server/index.js
@@ -25,6 +25,18 @@ app.get('/api/carousel/:restaurantID', (req, res) => {
   });
 });
 
+app.get('/api/carousel/:restaurantID/restaurant_name', (req, res) => {
+  const restaurant = req.params.restaurantID;
+  console.log('GET REQUEST recieved for restaurant ID of: ', restaurant);
+  Db.getRestaurantName(restaurant, (err, results) => {
+    if (err) {
+      console.log('err in server CB: ', err);
+    } else {
+      res.end(JSON.stringify(results));
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
