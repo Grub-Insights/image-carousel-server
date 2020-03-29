9da031dc652f4697cb6ef00a569eb17b219db5d1
EclecticTaco <calvi5210@gmail.com>
2020-03-29 10:43:56 -0700

    add SVG chevrons to main carousel

--- client/ImageCarouselArrows.jsx
+++ client/ImageCarouselArrows.jsx
@@ -4,15 +4,17 @@ import styles from './styles/ImageCarouselArrows'
 const ImageCarouselArrows = ({ direction, clickHandler, img }) => {
   if (direction === 'right') {
     return (
-      <span className={styles.right} onClick={clickHandler}>
-        {img}
-      </span>
+      <svg className={styles.right} onClick={clickHandler} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+      // // <span className={styles.right} onClick={clickHandler}>
+      //   {img}
+      // </span>
     );
   }
   return (
-  <span className={styles.left} onClick={clickHandler}>
-  {img}
-  </span>
+    <svg className={styles.left} onClick={clickHandler} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+  // <span className={styles.left} onClick={clickHandler}>
+  // {img}
+  // </span>
   );
   
 };
